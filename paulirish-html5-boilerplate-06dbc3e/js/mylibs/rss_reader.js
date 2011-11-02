08ce43f670303ef12e4cfabfc7460bb5f8943727
Sam Potasznik <sampotasznik@gmail.com>
2011-11-01 20:11:39 -0400

    Now have three buttons and can switch between different feeds!

--- paulirish-html5-boilerplate-06dbc3e/js/mylibs/rss_reader.js
+++ paulirish-html5-boilerplate-06dbc3e/js/mylibs/rss_reader.js
@@ -1,3 +1,45 @@
+function read_hogs_haven( feedIndex ) 
+{
+	const FOOTBALL_INSIDER_URL = 'http://feeds.washingtonpost.com/rss/rss_football-insider';
+	const HOGS_HAVEN_URL = 'http://feeds.feedburner.com/sportsblogs/hogshaven';
+	const BOG_URL = 'http://feeds.washingtonpost.com/rss/rss_dc-sports-bog';
+	
+	const ALL_FEEDS = [ FOOTBALL_INSIDER_URL, HOGS_HAVEN_URL, BOG_URL ];
+	
+	var url = ALL_FEEDS[ feedIndex ];
+	
+	$.jGFeed( url, function(feeds) 
+	{
+		// Check for errors
+		if(!feeds) {
+			// there was an error
+			return false;
+		}
+		
+		//clear the content in the div for the next feed.
+		$("#feedContent").empty();
+		
+		// do whatever you want with feeds here
+		for(var i = 0; i < feeds.entries.length; i++) {
+			var entry = feeds.entries[i];
+			// Entry title
+			var title = entry.title;
+			var pubDate = entry.publishedDate;
+			var description = entry.description;
+			var link = entry.link;
+			
+			var html = "<div class=\"entry\"><h2 class=\"postTitle\">" + title + "<\/h2>";
+			html += "<em class=\"date\">" + pubDate + "</em>";
+			html += "<p class=\"description\">" + description + "</p>";
+			html += "<a href=\"" + link + "\" target=\"_blank\">Read More >><\/a><\/div>";
+ 
+				//put that feed content on the screen!
+			$('#feedContent').append($(html));  
+		}
+	}, 10);
+};
+
+
 function get_rss_feed() {
 	//clear the content in the div for the next feed.
 	$("#feedContent").empty();
@@ -35,3 +77,4 @@ function get_rss_feed() {
 	});
  
 };
+
